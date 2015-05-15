d35fcbc61b37dc83715301379ab3101452f9b20c
mrmuffins <christopher.martinez@vohfix.com>
2015-05-15 18:33:29 -0500

    Exercise 2 completed

---
+++ exercises/exercise2.py
@@ -0,0 +1,8 @@
+# Begin Exercise 2 with comments in the beginning
+# All comments are started with an # 
+print "This is the comments exercise." # comment after
+
+# A comment can disable code as well 
+# print "This won't run"
+print "This will run"
+
