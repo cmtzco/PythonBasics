baf3f7c94403bffcacf92846428ac7a9c60598c5
mrmuffins <christopher.martinez@vohfix.com>
2015-05-15 19:23:39 -0500

    Exercise 5 completed

---
+++ exercises/exercise5.py
@@ -0,0 +1,18 @@
+my_name = 'Chris S. Martinez'
+my_age = 22 # just a youngin' from the streets
+my_height = 68 # inches
+my_weight = 200 # wow this got personal
+my_eyes = 'Brown'
+my_teeth = 'White' 
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy" # ain't that the damn truth!
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are ALWAYS %s." % my_teeth
+
+# it's about to get serious
+print "If I add %d, %d, and %d I get my %d." % (
+	my_age, my_height, my_weight, my_age + my_height + my_weight)
